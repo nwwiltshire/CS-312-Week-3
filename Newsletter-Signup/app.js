98b2e6492d557345154625097c517fe98b935b86
Nick Wiltshire <nwwiltshire@gmail.com>
2024-06-24 20:02:08 -0700

    Init

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on Heroku.");
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data)
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/b65a21d421"
+
+    const options = {
+        method: "POST",
+        auth: "nickw:349c0f95a1e7511606cd28d78069358c-us14"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+    
+});
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+});
+
+// API Key
+// 349c0f95a1e7511606cd28d78069358c-us14
+// List ID
+// b65a21d421
